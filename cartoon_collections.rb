ce412fff3ef21e73ad6d9e5ae02e1c7a0fc34463
Penelope Durand <penelope.durand@mail.goucher.edu>
2022-05-17 11:33:03 -0400

    commit tree a81df167621988da8adfa07b94316dc6a87fef4e

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
 def roll_call_dwarves# code an argument here
   # Your code here
+  dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"].each_with_index do |name, index|
+    puts "#{index}. #{name}"
+  end
 end
 
 def summon_captain_planet# code an argument here
